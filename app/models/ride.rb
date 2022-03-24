35e019ed6a2c9d9a26241863497f0158744af4a0
reuben <creubend@gmail.com>
2022-03-24 14:43:51 -0600

    test: Model Ride validates presence of type, seats, and full

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -1,3 +1,6 @@
 class Ride < ApplicationRecord
   has_many :passengers
+  validates :type, presence: true
+  validates :seats, presence: true
+  validates :full, presence: true
 end 
--- spec/models/ride_spec.rb
+++ spec/models/ride_spec.rb
@@ -1,5 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe Ride do 
-  it{should have_many :passengers}
+  it {should have_many :passengers}
+  it {should validate_presence_of(:type)}
+  it {should validate_presence_of(:seats)}
+  it {should validate_presence_of(:full)}
 end 
