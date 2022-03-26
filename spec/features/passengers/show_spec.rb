9569fb323b5fd1088fd585d04da1d80d7280ff7e
reuben <creubend@gmail.com>
2022-03-26 09:02:27 -0600

    test: Passenger show page test setup

---
+++ spec/features/passengers/show_spec.rb
@@ -0,0 +1,17 @@
+require "rails_helper"
+
+RSpec.describe 'passenger show page ' do 
+  it "lists the attributes of a passenger" do
+
+    camry = Ride.create!(name: 'Camry', seats: 4, full: true)
+    passenger1= camry.passengers.create!(name: "Tim", age: 19, single: true )
+    
+    visit "/passengers/#{passenger1.id}"
+    
+    save_and_open_page
+    expect(page).to have_content(passenger1.name)
+    expect(page).to have_content(passenger1.age)
+    expect(page).to have_content(passenger1.single)
+  end
+
+end 
