d21e088ffb97ccbda2deefdd9e8616d9c715aff2
reuben <creubend@gmail.com>
2022-03-27 11:57:40 -0600

    test: ride index page displays created at date

--- spec/features/rides/index_spec.rb
+++ spec/features/rides/index_spec.rb
@@ -1,17 +1,26 @@
 require "rails_helper"
 
 RSpec.describe 'rides index ' do 
-  describe "rides index" 
+   
     before :each do
       @camry = Ride.create!(name: 'Camry', seats: 4, full: true)
       @gt40 = Ride.create!(name: 'GT40', seats: 1, full: true)
       @accord = Ride.create!(name: 'Accord', seats: 4, full: true)
     end 
+    
     it "lists the type of each ride" do
       visit '/rides'
       expect(page).to have_content(@camry.name)
       expect(page).to have_content(@gt40.name)
       expect(page).to have_content(@accord.name)
+    end
+    
+    it "list when that record was created" do
+      visit '/rides'
+      expect(page).to have_content(@camry.created_at)
+      expect(page).to have_content(@gt40.created_at)
+      expect(page).to have_content(@accord.created_at)
+      
     end
   
 
