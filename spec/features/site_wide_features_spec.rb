9a96b7a0280c85e7d96ebfa5a48e88182c8b3e21
reuben <creubend@gmail.com>
2022-03-27 15:13:03 -0600

    feat: All pages display passengers and rides links

--- spec/features/site_wide_features_spec.rb
+++ spec/features/site_wide_features_spec.rb
@@ -8,14 +8,29 @@ RSpec.describe "features that are on every page" do
       camry = Ride.create!(name: 'Camry', seats: 4, full: true)
       passenger1 = camry.passengers.create!(name: "Tim", age: 19, single: true, created_at: "2022-03-27 17:16:19" )
 
-      pages = ["/rides", "/rides/:id", "/passengers", "/passengers/:id", "/rides/:id/passengers"]
+      pages = ["/rides", "/rides/#{camry.id}", "/passengers", "/passengers/#{passenger1.id}", "/rides/#{camry.id}/passengers"]
       
       pages.each do |page|
         visit page
         
-        click_on "Passenger"
+        click_on "Passengers"
         expect(current_path).to eq('/passengers') 
-      end   
+      end 
+    end
+    
+    it "has a link on every page back to Ride index page" do
+      
+      camry = Ride.create!(name: 'Camry', seats: 4, full: true)
+      passenger1 = camry.passengers.create!(name: "Tim", age: 19, single: true, created_at: "2022-03-27 17:16:19" )
+
+      pages = ["/rides", "/rides/#{camry.id}", "/passengers", "/passengers/#{passenger1.id}", "/rides/#{camry.id}/passengers"]
+      
+      pages.each do |page|
+        visit page
+        
+        click_on "Rides"
+        expect(current_path).to eq('/rides') 
+      end 
     end
     
      
