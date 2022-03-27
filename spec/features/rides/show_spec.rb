c2e857256af3796777d672175e402949093bd66a
reuben <creubend@gmail.com>
2022-03-27 15:33:11 -0600

    test: Test for link on rides page to passenger index

--- spec/features/rides/show_spec.rb
+++ spec/features/rides/show_spec.rb
@@ -23,5 +23,17 @@ RSpec.describe 'rides show page ' do
     visit "/rides/#{camry.id}"
     expect(page).to have_content("Number of passengers: #{camry.passengers.count}")
   end
+  
+  it "has a link to the ride passengers index" do
+    camry = Ride.create!(name: 'Camry', seats: 4, full: true)
+
+    passenger1= camry.passengers.create!(name: "Tim", age: 19, single: true, created_at: "2022-03-27 17:16:19" )
+    passenger2= camry.passengers.create!(name: "Becky", age: 37, single: true )
+    passenger3= camry.passengers.create!(name: "Rod", age: 30, single: true )
+
+    visit "/rides/#{camry.id}"
+    click_on "passengers"
+    expect(current_path).to eq("/rides/#{camry.id}/passengers")
+  end
 
 end 
